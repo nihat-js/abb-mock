c2dd7910e3ae6a9cb9d0b926030090fe2b8eb569
Nihat <abdullazadenihat@gmail.com>
2023-04-20 20:30:41 +0400

    added icons

--- task1/app.js
+++ task1/app.js
@@ -1,42 +1,42 @@
 const tabs = [
   {
     name: 'Web Design',
-    source: "",
+    source: "./icon1.svg",
     text: " lorem10 asperiores vel accusantium nesciunt voluptates accusamus quidem sed? Nobis deserunt, accusamus doloremque quisquam reprehenderit expedita magnam, pariatur quod fugiat at porro quo maxime. Quasi saepe accusamus eligendi laborum odio architecto  ",
     active: true,
 
   },
   {
     name: "Graphic Design",
-    source: "",
+    source: "./icon2.svg",
     text: "This is a graphic design",
     active: false,
 
   },
   {
     name: "Online Support ",
-    source: "",
+    source: "./icon3.svg",
     text: "Online support",
     active: false,
 
   },
   {
     name: 'App design',
-    source: "",
+    source: "./icon4.svg",
     text: "App Design ",
     active: false,
 
   },
   {
     name: "Online marketing",
-    source: "",
+    source: "./icon5.svg",
     text: "Online marketing",
     active: false,
 
   },
   {
     name: "Seo Service",
-    source: '',
+    source: "./icon6.svg",
     text: "Seo Service",
     active: false,
 
@@ -48,20 +48,27 @@ function renderServices() {
 
 }
 
+function changeActiveServiceTab(name) {
+  console.log("bura gel");
+  tabs.forEach(t => {
+    if (t.name == name) {
+      t.active = true
+    } else {
+      t.active = false
+    }
+  })
+  renderActiveServicesBox()
+  renderActiveServicesTab() /// for changin color
+}
+
+
 tabs.forEach(tab => {
   let p = document.createElement("p")
   p.innerText = tab.name
 
-  p.onclick = () => {
-    tabs.forEach(tab2 => {
-      if (tab2.name == tab.name) {
-          tab2.active = true
-      }else{
-        tab2.active = false
-      }
-    })
-    renderActiveServicesBox()
-  }
+  p.onclick = () => { changeActiveServiceTab(tab.name) }
+
+  renderActiveServicesBox()
   if (tab.active) {
     p.classList = "active"
   }
@@ -69,10 +76,14 @@ tabs.forEach(tab => {
 
 })
 
-function renderActiveServicesTab(){
-  tabs.forEach( tab => {
-    if (tab.active){
-      document.querySelector
+function renderActiveServicesTab() {
+  let activeTab = tabs.filter(x => x.active === true)[0]
+  let activeTabName = activeTab.name
+  document.querySelectorAll(".services  .tabs p").forEach(x => {
+    if (x.innerHTML == activeTabName) {
+      x.classList.add("active")
+    } else {
+      x.classList.remove("active")
     }
   })
 }
