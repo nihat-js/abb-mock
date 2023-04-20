a87fcb09fc595573d9497d1f42a2076e92fc6d99
Nihat <abdullazadenihat@gmail.com>
2023-04-20 20:37:46 +0400

    fixed task2 and task1

--- task2/app.js
+++ task2/app.js
@@ -1,26 +1,28 @@
 function dishes(receipe, ingredient) {
   let answer;
-  Object.keys(receipe).forEach((key) => {
+
+  for (const key in receipe) {
     if (!ingredient[key]) {
+      console.log("bura yoxladim")
       answer = 0
-      return false
+      break;
     }
-    console.log("gelme" , ingredient[key])
-    let count =  Math.floor (  ingredient[key] / receipe[key ] ) 
+
+    let count = Math.floor(ingredient[key] / receipe[key])
     if (!answer) {
       answer = count
     } else if (count < answer) {
       answer = count
     }
+  }
 
 
-  })
 
   return answer
 
 }
 
 
-let answer = dishes({ flourr: 500, sugar: 200, eggs: 1 }, { flour: 1200, sugar: 1200, eggs: 5, milk: 200 });
+let answer = dishes({ flour: 500, sugar: 200, eggs: 1 }, { flour: 1200, sugar: 1200, eggs: 5, milk: 200 });
 
 console.log("answer is " + answer)
